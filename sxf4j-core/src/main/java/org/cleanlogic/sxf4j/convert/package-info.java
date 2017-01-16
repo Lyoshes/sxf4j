e90b0daac5e53622a4cf35685c5ad3cbfcdde842
iserge <s.serge.b@gmail.com>
2017-01-16 15:17:08 +0300

    New package description

---
+++ sxf4j-core/src/main/java/org/cleanlogic/sxf4j/convert/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Coordinate converters package. {@link org.cleanlogic.sxf4j.convert.PROJ}, {@link org.cleanlogic.sxf4j.convert.GISCore}, {@link org.cleanlogic.sxf4j.convert.GOST}
+ * @author Serge Silaev aka iSergio <s.serge.b@gmail.com>
+ */
+package org.cleanlogic.sxf4j.convert;
---
+++ sxf4j-core/src/main/java/org/cleanlogic/sxf4j/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * SXF (Storage and eXchange Format) open format digital map information. Supports {@link org.cleanlogic.sxf4j.SXF#VERSION_3} AND {@link org.cleanlogic.sxf4j.SXF#VERSION_4}
+ * @author Serge Silaev aka iSergio <s.serge.b@gmail.com>
+ */
+package org.cleanlogic.sxf4j;
