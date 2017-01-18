97d32897fa5c7f9a3acd649980cfd170a8777223
iserge <s.serge.b@gmail.com>
2017-01-18 16:36:23 +0300

    Separate source coordinates and destination coordinates (if transform uses)

--- sxf4j-core/src/main/java/org/cleanlogic/sxf4j/format/SXFRecordMetric.java
+++ sxf4j-core/src/main/java/org/cleanlogic/sxf4j/format/SXFRecordMetric.java
@@ -11,57 +11,32 @@ import java.util.List;
  * @author Serge Silaev aka iSergio <s.serge.b@gmail.com>
  */
 public class SXFRecordMetric {
-    public Geometry geometry;
+    private final Geometry _srcGeometry;
+    private final Geometry _dstGeometry;
+
     public List<SXFRecordMetricText> metricTexts = new ArrayList<>();
 
-    public SXFRecordMetric() {}
-
-    public String geometryAsWKB() {
-        return geometryAsWKB(geometry);
+    public SXFRecordMetric(Geometry srcGeometry, Geometry dstGeometry) {
+        _srcGeometry = srcGeometry;
+        _dstGeometry = dstGeometry;
     }
 
-    public String geometryAsWKT() {
-        return geometryAsWKT(geometry);
+    public Geometry getSrcGeometry() {
+        return _srcGeometry;
     }
 
-    public String geometryAsWKT(boolean isFormatted) {
-        return geometryAsWKT(geometry, isFormatted);
+    public Geometry getDstGeometry() {
+        return _dstGeometry;
     }
 
-    public String geometryAsEWKT() {
-        return geometryAsEWKT(geometry);
-    }
-
-    public static String geometryAsWKT(Geometry geometry) {
-        return geometryAsWKT(geometry, false);
-    }
-
-    public static String geometryAsWKT(Geometry geometry, boolean isFormatted) {
-        WKTWriter wktWriter = new WKTWriter(3);
-        wktWriter.setFormatted(isFormatted);
-        return wktWriter.write(geometry);
-    }
-
-    public static String geometryAsEWKT(Geometry geometry) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("SRID=");
-        stringBuilder.append(geometry.getSRID());
-        stringBuilder.append(";");
-        stringBuilder.append(geometryAsWKT(geometry));
-        return stringBuilder.toString();
-    }
-
-    public static String geometryAsWKB(Geometry geometry) {
-        WKBWriter wkbWriter = new WKBWriter(3, true);
-        byte[] hex = wkbWriter.write(geometry);
-        String result = WKBWriter.toHex(hex);
-        if (geometry.isEmpty()) {
-            if (result.charAt(8) != 'A') {
-                StringBuilder sb = new StringBuilder(result);
-                sb.setCharAt(8, 'A');
-                result = sb.toString();
-            }
+    /**
+     * Function return optimal geometry. If source geometry reprojected - returns it, else returns source geometry
+     * @return
+     */
+    public Geometry getGeometry() {
+        if (_dstGeometry != null) {
+            return _dstGeometry;
         }
-        return result;
+        return _srcGeometry;
     }
 }
