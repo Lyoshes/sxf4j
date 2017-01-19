3cd56824a40e4d1da159f924405caa2ce4d78f34
iserge <s.serge.b@gmail.com>
2017-01-19 12:42:36 +0300

    Copyright update.

--- sxf4j-utils/bin/sxf2pgsql.sh
+++ sxf4j-utils/bin/sxf2pgsql.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 SXF4J=target/sxf4j-utils-0.0.1.jar
 
-#STARTTIME=$(date +%s)
+STARTTIME=$(date +%s)
 java -Xmx1024m -cp $SXF4J org.cleanlogic.sxf4j.utils.Sxf2Pgsql "$@"
-#ENDTIME=$(date +%s)
-#(>&2 echo "Time elapsed: $(($ENDTIME - $STARTTIME))")
+ENDTIME=$(date +%s)
+(>&2 echo "Time elapsed: $(($ENDTIME - $STARTTIME))")
