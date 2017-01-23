b433a85cc3683a2f396b4c6227dc7c582bd33814
iserge <s.serge.b@gmail.com>
2017-01-23 11:56:28 +0300

    Remove some fix

--- sxf4j-core/src/main/java/org/cleanlogic/sxf4j/fixes/SXFRecordSemanticFix.java
+++ sxf4j-core/src/main/java/org/cleanlogic/sxf4j/fixes/SXFRecordSemanticFix.java
@@ -22,20 +22,6 @@ import org.cleanlogic.sxf4j.format.SXFRecordSemantic;
  * @author Serge Silaev aka iSergio <s.serge.b@gmail.com>
  */
 public class SXFRecordSemanticFix {
-    public static SXFRecordSemantic ScaleValue(SXFRecordSemantic sxfRecordSemantic) {
-        switch (sxfRecordSemantic.type) {
-            case STRDOS:
-            case STRING:
-            case STRUNI:
-            case STRUTF16:
-                if (sxfRecordSemantic.scale < 0) {
-                    sxfRecordSemantic.scale *= -1;
-                }
-                break;
-            default: break;
-        }
-        return sxfRecordSemantic;
-    }
 
     public static String RemoveBinaryZero(byte[] string) {
         String result = "";
